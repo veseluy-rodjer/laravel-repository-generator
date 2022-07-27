9e40deef991d89834cac6604b5d4605be5cbc33a
Nikolay <nikolas-ja@meta.ua>
2022-07-27 13:28:58 +0300

    get Collection

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -3,13 +3,13 @@
 namespace VeseluyRodjer\RepositoryGenerator\Repository;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 
 class BaseRepository
 {
-    public function getAll(): Builder
+    public function getAll(): Collection
     {
-        return $this->model->newQuery()->getAll();
+        return $this->model->newQuery()->getAll()->get();
     }
 
     public function create(array $attributes): Model
@@ -17,33 +17,33 @@ class BaseRepository
         return $this->model->create($attributes);
     }
 
-    public function findModel(int $id): Builder
+    public function findModel(int $id): Model
     {
-        return $this->model->newQuery()->findModel($id);
+        return $this->model->newQuery()->findModel($id)->first();
     }
 
-    public function filter(string $attr, string|int $val): Builder
+    public function filter(string $attr, string|int $val): Collection
     {
-        return $this->model->newQuery()->filter($attr, $val);
+        return $this->model->newQuery()->filter($attr, $val)->get();
     }
 
     public function update(array $attributes, int $id): bool
     {
-        return $this->filter('id', $id)->update($attributes);
+        return $this->model->newQuery()->filter('id', $id)->update($attributes);
     }
 
     public function delete(int $id): bool
     {
-        return $this->filter('id', $id)->delete();
+        return $this->model->newQuery()->filter('id', $id)->delete();
     }
 
-    public function statusFilter(int $status): Builder
+    public function statusFilter(int $status): Collection
     {
-        return $this->model->newQuery()->statusFilter($status);
+        return $this->model->newQuery()->statusFilter($status)->get();
     }
 
-    public function filterByCompare(string $attr, string $compare, string|int $val): Builder
+    public function filterByCompare(string $attr, string $compare, string|int $val): Collection
     {
-        return $this->model->newQuery()->filterByCompare($attr, $compare, $val);
+        return $this->model->newQuery()->filterByCompare($attr, $compare, $val)->get();
     }
 }
