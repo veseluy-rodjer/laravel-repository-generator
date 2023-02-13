f7fc4aa9bed35443eb991fecdd8cfdd07054b3dc
Nikolay <nikolas-ja@meta.ua>
2023-02-13 18:29:24 +0200

    return builder

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -3,13 +3,13 @@
 namespace VeseluyRodjer\RepositoryGenerator\Repository;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 class BaseRepository
 {
-    public function getAll(): Collection
+    public function getAll(): Builder
     {
-        return $this->model->newQuery()->getAll()->get();
+        return $this->model->newQuery()->getAll();
     }
 
     public function create(array $attributes): Model
@@ -22,9 +22,9 @@ class BaseRepository
         return $this->model->newQuery()->findModel($id)->first();
     }
 
-    public function filter(string $attr, string|int $val): Collection
+    public function filter(string $attr, string|int $val): Builder
     {
-        return $this->model->newQuery()->filter($attr, $val)->get();
+        return $this->model->newQuery()->filter($attr, $val);
     }
 
     public function update(array $attributes, int $id): bool
@@ -37,13 +37,13 @@ class BaseRepository
         return $this->model->newQuery()->filter('id', $id)->delete();
     }
 
-    public function statusFilter(int $status): Collection
+    public function statusFilter(int $status): Builder
     {
-        return $this->model->newQuery()->statusFilter($status)->get();
+        return $this->model->newQuery()->statusFilter($status);
     }
 
-    public function filterByCompare(string $attr, string $compare, string|int $val): Collection
+    public function filterByCompare(string $attr, string $compare, string|int $val): Builder
     {
-        return $this->model->newQuery()->filterByCompare($attr, $compare, $val)->get();
+        return $this->model->newQuery()->filterByCompare($attr, $compare, $val);
     }
 }
