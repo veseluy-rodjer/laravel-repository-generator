3f4dd84463a97892c8c431c7fe436a2859697ab5
Nikolay <nikolas-ja@meta.ua>
2023-08-18 19:18:54 +0300

    add methods

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -27,14 +27,43 @@ class BaseRepository
         return $this->model->newQuery()->filter($attr, $val);
     }
 
+    public function filterIn(string $attr, array $data): Builder
+    {
+        return $this->model->newQuery()->filterIn($attr, $data);
+    }
+
+    public function modelWith(string $relationship, int $id): Builder
+    {
+        return $this->model->newQuery()->modelWith($relationship, $id);
+    }
+
+    public function allWith(string $relationship): Builder
+    {
+        return $this->model->newQuery()->allWith($relationship);
+    }
+
     public function update(array $attributes, int $id): bool
     {
         return $this->model->newQuery()->filter('id', $id)->update($attributes);
     }
 
+    public function massUpdate(array $attributes, string $field, array $data): bool
+    {
+        return $this->model->newQuery()
+            ->filterIn($field, $data)
+            ->update($attributes);
+    }
+
     public function delete(int $id): bool
     {
         return $this->model->newQuery()->filter('id', $id)->delete();
+    }
+
+    public function massDelete(string $field, array $data): bool
+    {
+        return $this->model->newQuery()
+            ->filterIn($field, $data)
+            ->delete();
     }
 
     public function statusFilter(int $status): Builder
@@ -45,5 +74,10 @@ class BaseRepository
     public function filterByCompare(string $attr, string $compare, string|int $val): Builder
     {
         return $this->model->newQuery()->filterByCompare($attr, $compare, $val);
+    }
+
+    public function sort(string $attr, string $direction): Builder
+    {
+        return $this->model->newQuery()->sort($attr, $direction);
     }
 }
